93d63488893fa211bdd78f1b409f1b9b1ac3644b
Michael Hall <mbhall88@gmail.com>
2020-03-18 17:01:35 +0000

    add single to multi fast5 script

---
+++ shell/single_to_multi_fast5.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# https://github.com/nanoporetech/ont_fast5_api#single_to_multi_fast5
+
+if [[ $# -lt 2 || $# -gt 5 ]]; then
+	echo "Error: Illegal number of parameters: $#"
+	echo "Usage: $0 <indir> <outdir> [<batch_size> <filename_base> <threads>]"
+	exit 2
+fi
+
+set -eux
+
+# make sure singularity v3 is being used
+module load singularity/3.5.0
+
+container="docker://quay.io/biocontainers/ont-fast5-api:3.0.1--py_0"
+
+indir="$1"
+if [ ! -d "$indir" ]; then
+	echo "Error: fast5 input directory does not exist: $indir"
+	exit 1
+fi
+
+outdir="$2"
+if [ ! -d "$outdir" ]; then
+	echo "Output directory $outdir does not exist. Creating..."
+	mkdir "$outdir"
+fi
+
+batch_size="${3:-4000}"
+
+filename_base="${4:-batch}"
+
+threads="${5:-1}"
+
+singularity exec "$container" single_to_multi_fast5 --recursive \
+	--input_path "$indir" \
+	--save_path "$outdir" \
+	--threads "$threads" \
+	--batch_size "$batch_size" \
+	--filename_base "$filename_base"
